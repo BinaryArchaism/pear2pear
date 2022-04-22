810205d93729ed622f42600b8df8a006d467556f
Dmitriev.Dmitriy14 <dmitriev.dmitriy14@wb.ru>
2022-04-22 23:14:27 +0300

    mvp

---
+++ src/Components/wallet/ApproveSendmentByBuyer.js
@@ -0,0 +1,26 @@
+import {Contract, ethers} from "ethers";
+import detectEthereumProvider from "@metamask/detect-provider/dist/detect-provider";
+import Pear2PearNoMediator from "./contracts/Pear2PearNoMediator.json"
+import SessionId from "./EventCatch";
+
+const ApproveSendmentByBuyer = async () => {
+    console.log("lets ApproveSendmentByBuyer")
+    let provider = await detectEthereumProvider();
+    if(!provider) {
+        return
+    }
+    await provider.request({ method: 'eth_requestAccounts' });
+    //const networkId = await provider.request({ method: 'net_version' })
+    provider = new ethers.providers.Web3Provider(provider);
+    const signer = provider.getSigner();
+    const pear2PearNoMediator = new Contract(
+        "0xD871A341fE03823D62E2f09f87eE54404Bd3484B",
+        Pear2PearNoMediator.abi,
+        signer
+    );
+    console.log(SessionId.sessionId);
+
+    let tx = await pear2PearNoMediator.approveBuyer(SessionId.sessionId);
+    await tx.wait();
+}
+export default ApproveSendmentByBuyer;
---
+++ src/Components/wallet/EventCatch.js
@@ -0,0 +1,3 @@
+export default class SessionId {
+    static sessionId = 7;
+}
